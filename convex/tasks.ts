843421ef65013cca0c60cdf937513215016aa502
Yemaj W <ywabar3@gmail.com>
2025-04-19 02:23:14 -0400

    Updated deployed convex url

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -1,8 +1,16 @@
-import { query } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("tasks").collect();
+  },
+});
+
+export const create = mutation({
+  args: { text: v.string() },
+  handler: async (ctx, { text }) => {
+    return await ctx.db.insert("tasks", { text });
   },
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
 "use client";
-import { useQuery } from "convex/react";
+import { useQuery, useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
+  const createTask = useMutation(api.tasks.create);
+
+  const addNewTask = async () => {
+    const input = document.querySelector(
+      "input[type='text']"
+    ) as HTMLInputElement;
+    if (input && input.value) {
+      await createTask({ text: input.value });
+      input.value = "";
+    }
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addNewTask();
+        }}
+        className="flex"
+      >
+        <input
+          type="text"
+          placeholder="Task"
+          className="border-2 border-gray-300 rounded-md p-2"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white rounded-md p-2 ml-2"
+        >
+          Add Task
+        </button>
+      </form>
     </main>
   );
 }
